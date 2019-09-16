4841dff17704cca2be91e7ed4180db9e490807c3
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-09-16 19:04:09 -0400

    only got simple tests to work

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,27 @@
-// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+describe('describe proper colors', () => {
+    it('displayed and green', () => {
+        const display = render(<Display closed={false} locked={false} />)
+        display.getByText('open')
+        display.getByText('Unlocked')
+
+        expect(display.getByText('open').className).toContain('green-led')
+        expect(display.getByText('Unlocked').className).toContain('green-led')
+    })
+    it('have a green unlock and a red closed', () => {
+        const display = render(<Display closed={true} locked={false}/>)
+
+        display.etByText('closed')
+        display.getByText('Unlocked')
+
+        expect(display.getByText('closed').className).toContain('red-led')
+        expect(display.getByText('Unlocked').className).toContain('green-led')
+
+    })
+})
+
+
